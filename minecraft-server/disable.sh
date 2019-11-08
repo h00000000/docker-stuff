3ce8b5129c822ea141ff581a187bc23af402faf7
Florian Hübner <fhuebner@posteo.de>
2019-11-08 21:24:04 +0100

    cd if needed

--- minecraft-server/disable.sh
+++ minecraft-server/disable.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ ! "$PWD" = "$SCRIPTDIR" ];
+then
+  cd "$SCRIPTDIR" || exit
+fi
+
 docker-compose stop -t 60
 docker-compose rm -f
 
--- minecraft-server/enable.sh
+++ minecraft-server/enable.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-docker-compose stop -t 60
-docker-compose rm -f
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ ! "$PWD" = "$SCRIPTDIR" ];
+then
+  cd "$SCRIPTDIR" || exit
+fi
+
+./disable.sh
 
 mkdir -p /opt/minecraft/plugins/bStats/
 echo "enabled: false" > /opt/minecraft/plugins/bStats/config.yml
