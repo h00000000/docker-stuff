41727d4f7e2b249092f3df29f1fa29e964f6be4b
Florian Hübner <fhuebner@posteo.de>
2019-11-09 10:03:20 +0100

    google cadvisor

---
+++ cadvisor/enable.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ ! "$PWD" = "$SCRIPTDIR" ];
+then
+  cd "$SCRIPTDIR" || exit
+fi
+
+./disable.sh
+
+docker-compose --compatibility up -d --no-recreate
+docker-compose ps
+
+ufw allow 8080/tcp
